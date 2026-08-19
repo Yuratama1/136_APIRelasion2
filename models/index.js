64cf898c72be2a7cbe1011f286ca87bcae3643a6
Yuratama1 <yuratama661@gmail.com>
2026-08-19 10:45:49 +0700

    update index.js

--- models/index.js
+++ models/index.js
@@ -11,9 +11,30 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  const connectionurl = process.env[config.use_env_variable];
+
+  const url = new URL(connectionurl);
+
+  url.searchParams.delete('sslmode');
+
+  sequelize = new Sequelize(url.toString(), {
+    ...config,
+    dialect: "postgres",
+    dialectModule: PG,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database, 
+    config.username, 
+    config.password, 
+    config
+  );
 }
 
 fs
